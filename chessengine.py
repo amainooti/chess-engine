100187ee2cf3723da1d9f0524026bc347a7a04bc
amainooti <amainooti@gmail.com>
2023-12-30 10:58:11 +0100

    arrange the pieces and create move logic

---
+++ chessengine.py
@@ -0,0 +1,57 @@
+"""
+    This is responsible for storing the information about the current state of a game. It will
+    also be responsible for determining the valid moves at the current state, it'll keep a move log
+"""
+
+class Gamestate:
+    def __init__(self):
+        #the board is an 8X8 2d list, each has 2 characters
+        # the first character reps the color of the piece 'b' or 'w'
+        # the second character reps the type of the piece 'k', 'q', 'r', 'b', 'N', or 'p'
+        # '--' represents empty space with no piece.
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
+        ]
+
+        self.whiteToMove = True
+        self.moveLog = []
+
+        def makeMove(self, move):
+            pass
+
+
+
+class Move:
+    ranks_to_rows = {
+        "1": 7, "2": 6 , "3": 5 , "4": 4 , "5": 3,
+        "6": 2, "7": 1, "8": 0,
+    }
+    rows_to_ranks = {v:k for k, v in ranks_to_rows.items()}
+    files_to_col = {
+        "a": 0, "b": 1 , "c": 2 , "d": 3 , "e": 4,
+        "f": 5, "g": 6, "h": 7,
+    }
+    cols_to_files = {v:k for k, v in files_to_col.items()}
+
+    def __init__(self, startSq, endSq, board)-> None:
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+
+    def get_chess_notations(self):
+        return self.get_rank_files(startRow, startCol) + self.get_rank_files(self.endRow, self.endCol)
+
+
+    def get_rank_files(self, r, c):
+        return self.cols_to_files[c] + self.rows_to_ranks[r]
+
---
+++ chessmain.py
@@ -0,0 +1,96 @@
+"""
+    This is the main driver file, it's responsible for handling user input and displaying current
+    game state object.
+"""
+
+import pygame as p
+from chessengine import Gamestate
+
+# variables declarations
+p.init()
+WIDTH = HEIGHT = 512
+DIMENSION = 8 # dimension of the chess board 8x8
+SQ_SIZE = HEIGHT // DIMENSION #square size of the chess board
+MAX_FPS = 15
+IMAGES = {}
+
+'''
+    loading into memory is an expensive operation, so we only want to load it once and not
+    every frame
+
+
+    initialize a global variable of images which will be called exactly once in the main
+'''
+
+def loadImages():
+    pieces = ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR", "wp", "bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR", "bp"]
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load(f"images/{piece}.png"), (SQ_SIZE, SQ_SIZE))
+
+    # NOTE YOU CAN ACCESS AN IMAGE BY IMAGES['wp']
+
+
+"""
+the main driver for our code, this will handle user input and updating the graphics
+"""
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = Gamestate()
+    loadImages()  # only do this once
+    running = True
+    sqSelected = ()
+    playerClicks = []  # keeps track of the player clicks, this is a tuple: [(6, 4)]
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                x, y = p.mouse.get_pos()
+                col = x // SQ_SIZE
+                row = y // SQ_SIZE
+                if sqSelected == (row, col):  # undo selection
+                    sqSelected = ()
+                    playerClicks = []
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected)
+                if len(playerClicks) == 2:
+                    ...
+
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+'''
+Responsible for all the graphics within a current game state
+'''
+
+
+def drawBoard(screen):
+    colors = p.Color("white"), p.Color("dark green")
+    # draw pieces on the board using the current GameState.board
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r + c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--": #not an empty square
+                screen.blit(IMAGES[piece], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+def drawGameState(screen, gs):
+    drawBoard(screen) # draw squares on the board
+    drawPieces(screen, gs.board) # draw
+
+if __name__ == "__main__":
+    main()
